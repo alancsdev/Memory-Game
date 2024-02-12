709ead80b5f1af6aef96fb472991a66303892c50
Alan Santos <alanchs@live.com>
2024-02-12 11:33:11 +0800

    readme changes

--- js/main.js
+++ js/main.js
@@ -188,7 +188,7 @@ checkCards = () => {
         const cardInner =
           elementsWithDataCardValue.querySelector('.card-inner');
         cardInner.classList.remove('flip');
-        if (play === true && wrongChoices > 6) {
+        if (play === true && wrongChoices > 7) {
           gameOver();
         }
       });
